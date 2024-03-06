b3b20fd2cdeaab07d3f953d707318138f2b008bf
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2024-03-06 19:13:24 -0300

    Fix: lambda permission

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -15,18 +15,18 @@ resource "aws_lambda_function" "create_spot_instance_lambda" {
   }
 }
 
-resource "aws_lambda_alias" "create_spot_instance_lambda_alias" {
-  name             = "create_spot_instance_lambda_alias"
-  description      = "create_spot_instance_lambda_alias"
-  function_name    = aws_lambda_function.create_spot_instance_lambda.function_name
-  function_version = "$LATEST"
-}
+# resource "aws_lambda_alias" "create_spot_instance_lambda_alias" {
+#   name             = "create_spot_instance_lambda_alias"
+#   description      = "create_spot_instance_lambda_alias"
+#   function_name    = aws_lambda_function.create_spot_instance_lambda.function_name
+#   function_version = "$LATEST"
+# }
 
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
+resource "aws_lambda_permission" "allow_cloudwatch_alarm_to_invoke_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatchAlarm"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.create_spot_instance_lambda.function_name
-  principal     = "events.amazonaws.com"
+  principal     = "lambda.alarms.cloudwatch.amazonaws.com"
   source_arn    = var.cloudwatch_sqs_queue_alarm_arn
-  qualifier =   aws_lambda_alias.create_spot_instance_lambda_alias.name
+  # qualifier =   aws_lambda_alias.create_spot_instance_lambda_alias.name
 }
