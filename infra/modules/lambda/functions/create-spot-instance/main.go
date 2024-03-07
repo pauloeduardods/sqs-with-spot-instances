244c201d00909cd06131b15502beabda81d84bed
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2024-03-07 16:53:34 -0300

    Feat: Add logger util

--- infra/modules/lambda/functions/create-spot-instance/main.go
+++ infra/modules/lambda/functions/create-spot-instance/main.go
@@ -2,33 +2,26 @@ package main
 
 import (
 	"context"
+	"create-spot-instance/utils/Logger"
 	"encoding/json"
 	"fmt"
-	"log"
 
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
-const (
-	LOG_ERROR   = "Error"
-	LOG_INFO    = "Info"
-	LOG_WARNING = "Warning"
-
-	LOG_HANDLE_REQUEST = "HandleRequest"
-)
+var logger = Logger.NewLogger()
 
 func HandleRequest(ctx context.Context, event json.RawMessage) (string, error) {
-	log.Printf("%s %s: Event received: %s\n", LOG_HANDLE_REQUEST, LOG_INFO, event)
+	logger.Info("Event received: %s", event)
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("us-east-1"),
 	)
 	if err != nil {
-		log.Printf("%s %s: Error loading AWS configuration: %v\n", LOG_HANDLE_REQUEST, LOG_ERROR, err)
-		return "", fmt.Errorf("%s %s: Error loading AWS configuration: %v", LOG_HANDLE_REQUEST, LOG_ERROR, err)
+		logger.Error("Error loading AWS configuration: %v", err)
+		return "", fmt.Errorf("Error loading AWS configuration: %v", err)
 	}
 
 	client := sqs.NewFromConfig(cfg)
@@ -42,21 +35,22 @@ func HandleRequest(ctx context.Context, event json.RawMessage) (string, error) {
 		},
 	})
 	if err != nil {
-		log.Printf("%s %s: Error getting queue attributes: %v\n", LOG_HANDLE_REQUEST, LOG_ERROR, err)
-		return "", fmt.Errorf("%s %s: Error getting queue attributes: %v", LOG_HANDLE_REQUEST, LOG_ERROR, err)
+		logger.Error("Error getting queue attributes: %v", err)
+		return "", fmt.Errorf("Error getting queue attributes: %v", err)
 	}
 
 	numMessages, ok := result.Attributes["ApproximateNumberOfMessages"]
 	if !ok {
-		log.Printf("%s %s: Error getting queue attributes: ApproximateNumberOfMessages not found\n", LOG_HANDLE_REQUEST, LOG_ERROR)
-		return "", fmt.Errorf("%s %s: Error getting queue attributes: ApproximateNumberOfMessages not found", LOG_HANDLE_REQUEST, LOG_ERROR)
+		logger.Error("Error getting queue attributes: ApproximateNumberOfMessages not found")
+		return "", fmt.Errorf("Error getting queue attributes: ApproximateNumberOfMessages not found")
 	}
 
-	log.Printf("%s %s: Event processed successfully. Approximate number of messages in the queue: %s\n", LOG_HANDLE_REQUEST, LOG_INFO, numMessages)
+	logger.Info("Approximate number of messages in the queue: %s", numMessages)
 
 	return fmt.Sprintf("Approximate number of messages in the queue: %s", numMessages), nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	HandleRequest(context.Background(), nil)
+	// lambda.Start(HandleRequest)
 }
