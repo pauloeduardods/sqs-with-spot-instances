a39c10a83e46c12049f24e668bfff960894e28d7
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2024-04-05 18:15:15 -0300

    Refac: Remove old lambda

--- infra/modules/lambda/functions/resize-asg/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_function" {
-  description = "Lambda function to resize ASG"
-  value       = {
-    name = aws_lambda_function.lambda_function.function_name
-    arn  = aws_lambda_function.lambda_function.arn
-  }
-}
